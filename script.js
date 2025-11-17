0a05497386149969ccc5bdd646c18f550f60689d
rimka1312 <rimkaq3316@gmail.com>
2025-11-17 21:11:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,8 @@
 const { createClient } = supabase;
 
 // الخطوة 2: مفاتيحك الخاصة (استبدلها من حسابك)
-const SUPABASE_URL = 'https://vhrvdkaqlrwplkdgwwkl.supabase.co'; // ⬅️ غيّر هذا
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZocnZka2FxbHJ3cGxrZGd3d2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTUyMTAsImV4cCI6MjA3ODk3MTIxMH0.mNAn3qo48y46FDkDOqUVt1xwN2smFMZL1lBNbT0OkTA'; // ⬅️ غيّر هذا
-
+const SUPABASE_URL = 'https://vhrvdkaqlrwplkdgwwkl.supabase.co'; // ⬅️ (هذا سيكون الرابط الخاص بك)
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZocnZka2FxbHJ3cGxrZGd3d2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTUyMTAsImV4cCI6MjA3ODk3MTIxMH0.mNAn3qo48y46FDkDOqUVt1xwN2smFMZL1lBNbT0OkTA'; // ⬅️ (هذا سيكون المفتاح الطويل الخاص بك)
 // الخطوة 3: إنشاء "العميل" باسم جديد "supabaseClient"
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
